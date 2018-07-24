d0b6c946da9db5c4146853992f406d6d41e06c82
Unknown <youssef.soliman.1@etsmtl.net>
2018-07-24 17:42:50 -0400

    revert trajectoire_ecran.h

--- TP3/TP3/trajectoire_ecran.h
+++ TP3/TP3/trajectoire_ecran.h
@@ -16,77 +16,20 @@
 
 	 En tout dernier vient un type dédié au regroupement de trajectoires
 	 écran, implémenté dans un tableau dynamique dont la taille peut varier.
-
-	 Les fonctions définent dans ce module sont :
-
-	 - t_trajectoire_ecran init_trajectoire_ecran(void);
-	   (construire une trajectoire vide)
-
-	- int taille_trajectoire_ecran(const t_trajectoire_ecran * traj);
-	  ( obtenir le nombre de points écran actuellement dans la trajectoire )
- ) 
-
-	- int trajectoire_ecran_vide(const t_trajectoire_ecran * traj);
-	  ( savoir si la trajectoire est vide )
-
-	- void set_iter_debut(t_trajectoire_ecran * traj);
-	  ( mettre le pointeur-itérateur (position courant) au début de la file )
-
-	- t_point_ecran get_point_iter(t_trajectoire_ecran * traj);
-	  ( obtenir le point écran du pointeur-itérateur et avancer ce pointeur 
-	  au prochain noeud et sert dans dessiner la trajectoire )
-
-	- int enfiler_point_ecran(t_trajectoire_ecran * traj, const t_point_ecran pt);
-	  ( ajoute un noeud contenant un point_ecran à la fin de la trajectoire )
-
-	- int lire_trajectoire_ecran(t_trajectoire_ecran * traj);  
-	 ( construire une nouvelle trajectoire obtenue du mouvement continu de 
-	   la souris utiliser mode_graphique.h pour le réaliser )
-    
-	- void dessiner_trajectoire_ecran(t_trajectoire_ecran * traj, int couleur);  
-	  ( dessiner une  trajectoire dans l'écran graphique utiliser 
-	    mode_graphique.h pour le réaliser )
-
-	- int liberer_trajectoire_ecran(t_trajectoire_ecran * traj);
-	  ( supprime tous les noeuds de la trajectoire ) 
-
-	- t_groupe_traj_ecran init_groupe(int taille);
-	  ( Creer un groupe initialement vide mais capable de conserver
-        un nombre de trajectoires donné en parametre ) 
-  
-     - int get_nombre_traj_groupe(const t_groupe_traj_ecran * groupe);
-       ( Retourne le nombre actuel de trajectoires  dans le groupe )
-	
-	 - int get_taille_groupe(const t_groupe_traj_ecran * groupe);
-	   ( Retourne la taille actuelle du tableau dynamique du groupe )
-
-	  - t_trajectoire_ecran  consulter_groupe(const t_groupe_traj_ecran
-	     * groupe, int position);
-		 (Si la position reçue est correcte, une copie de la trajectoire
-           à cette position est retournée)
-
-	  - int ajouter_traj_groupe(t_groupe_traj_ecran * groupe, 
-	     t_trajectoire_ecran * traj);
-		 ( Si une place est disponible dans le tableau de ce groupe, 
-           on ajoute la trajectoire )
-
-	 - int vider_traj_groupe(t_groupe_traj_ecran * groupe);
-	   ( Supprime toutes les trajectoires du groupe )
 */
-
-/*=========================================================*/
-/*                  LES CONSTANTES                         */
-/*=========================================================*/
-
 
 /* le nombre de trajectoires à saisir dans un groupe */
 #define NB_TRAJECTOIRES  6
 
-
-/*=========================================================*/
-/*                       STRUCTURE                         */
-/*=========================================================*/
-
+/*****************************************************/
+/*
+   Définition d'un point de l'écran.
+   Caractéristique importante : leurs coordonnées sont en pixels
+   on les conserve dans deux entiers.
+   NOTE: ce type est considéré "publique" pour tous. On peut donc
+   faire directement accès à ses 2 champs dans n'importe quel autre
+   module ou fichier-client.
+*/
 typedef struct {
 	// la position-pixel du point
 	int pos_x;   // coordonnée horizontale
@@ -138,7 +81,7 @@ t_trajectoire_ecran init_trajectoire_ecran(void);
 /* INFORMATRICE
 obtenir le nombre de points écran actuellement dans la trajectoire
 
-PARAMETRE : * traj : l'adresse de la trajectoire
+PARAMETRE : l'adresse de la trajectoire
 
 SORTIE: le nombre d'elements dans la file
 
@@ -150,7 +93,7 @@ int taille_trajectoire_ecran(const t_trajectoire_ecran * traj);
 /* INFORMATRICE
 savoir si la trajectoire est vide
 
-PARAMETRE : * traj : l'adresse de la trajectoire
+PARAMETRE : l'adresse de la trajectoire
 
 SORTIE: 1 si la file est vide, 0 sinon
 
@@ -162,7 +105,7 @@ int trajectoire_ecran_vide(const t_trajectoire_ecran * traj);
 /* MUTATRICE
 mettre le pointeur-itérateur (position courant) au début de la file.
 
-PARAMETRE : * traj : l'adresse de la trajectoire
+PARAMETRE : l'adresse de la trajectoire
 
 SORTIE: aucune
 */
@@ -173,7 +116,7 @@ void set_iter_debut(t_trajectoire_ecran * traj);
 obtenir le point écran du pointeur-itérateur et avancer ce pointeur au prochain noeud.
 sert dans dessiner la trajectoire
 
-PARAMETRE : * traj : l'adresse de la trajectoire
+PARAMETRE : l'adresse de la trajectoire
 
 SORTIE: un t_point_ecran
 
@@ -186,8 +129,7 @@ t_point_ecran get_point_iter(t_trajectoire_ecran * traj);
 /* MUTATRICE
 ajoute un noeud contenant un point_ecran à la fin de la trajectoire (FIFO)
 
-PARAMETRES :- * traj : l'adresse de la trajectoire
-	        - pt : l'adresse du point_ecran à ajouter
+PARAMETRES : l'adresse de la trajectoire, l'adresse du point_ecran à ajouter
 
 SORTIE: 1 si l'ajout du noeud est fait, 0 sinon
 */
@@ -197,8 +139,7 @@ int enfiler_point_ecran(t_trajectoire_ecran * traj, const t_point_ecran pt);
 /* MUTATRICE
 élimine le premier noeud de la trajectoire et copie le point écran dans la référence (FIFO)
 
-PARAMETRES :- * traj : l'adresse de la trajectoire
-			- * pt : la référence prête à recevoir le t_point_ecran extrait
+PARAMETRES : l'adresse de la trajectoire, la référence prête à recevoir le t_point_ecran extrait
 
 SORTIE: 0 si la trajectoire était vide et 1 sinon.
 */
@@ -209,7 +150,7 @@ int defiler_point_ecran(t_trajectoire_ecran * traj,  t_point_ecran * pt);
  construire une nouvelle trajectoire obtenue du mouvement continu de la souris
  utiliser mode_graphique.h pour le réaliser
 
-PARAMETRE : * traj : la référence prête à recevoir la nouvelle trajectoire
+PARAMETRE : la référence prête à recevoir la nouvelle trajectoire
 
 SORTIE: si la trajectoire est valide, le nombre de points saisis et 0 sinon.
 */
@@ -220,8 +161,7 @@ int lire_trajectoire_ecran(t_trajectoire_ecran * traj);
 dessiner une  trajectoire dans l'écran graphique
 utiliser mode_graphique.h pour le réaliser
 
-PARAMETRES :- * traj : l'adresse de la trajectoire
-            - couleur : la couleur d'affichage désirée
+PARAMETRES : l'adresse de la trajectoire, la couleur d'affichage désirée
 
 SORTIE: Rien
 */
@@ -230,7 +170,7 @@ void dessiner_trajectoire_ecran(t_trajectoire_ecran * traj, int couleur);
 /*****************************************************/
 /* DESTRUCTEUR supprime tous les noeuds de la trajectoire
 
-PARAMETRE: * traj : l'adresse de la trajectoire,
+PARAMETRE: l'adresse de la trajectoire,
 
 SORTIE: le nombre de noeuds supprimés
 
@@ -264,7 +204,7 @@ typedef struct {
 Creer un groupe initialement vide mais capable de conserver
 un nombre de trajectoires donné en parametre.
 
-PARAMETRE : taille : la taille du tableau dynamique voulu
+PARAMETRE : la taille du tableau dynamique voulu
 
 SORTIE: le nouveau groupe de trajectoires 
 */
@@ -291,8 +231,7 @@ t_trajectoire_ecran  consulter_groupe(const t_groupe_traj_ecran * groupe, int po
 Si une place est disponible dans le tableau de ce groupe, 
 on ajoute la trajectoire.
 
-PARAMETRES:- * groupe : l'adresse du groupe
-		   - *traj : l'adresse de la trajectoire à ajouter
+PARAMETRES: l'adresse du groupe, l'adresse de la trajectoire à ajouter
     
 SORTIE: de 0 ou 1
 */
@@ -302,7 +241,7 @@ int ajouter_traj_groupe(t_groupe_traj_ecran * groupe, t_trajectoire_ecran * traj
 /* DESTRUCTEUR 
 Supprime toutes les trajectoires du groupe.
 
-PARAMETRE: * groupe : l'adresse du groupe
+PARAMETRE: l'adresse du groupe
 
 SORTIE: le nombre de trajectoires supprimées
 
