66a3f19dd8810545373f8bd00fbefa4a8e297779
Unknown <youssef.soliman.1@etsmtl.net>
2018-07-24 12:40:08 -0400

    implementation definition: retirer_traj_refuse()

--- TP3/TP3/trajectoire_plan.cpp
+++ TP3/TP3/trajectoire_plan.cpp
@@ -30,7 +30,60 @@ int trouver_traj_refuse(const t_liste_traj * listes_traj) {
 }
 
 void retirer_traj_refuse(t_liste_traj * listes_traj, int pos) {
+	//pour la position du noeud lorsqu'on ne choisi pas le premier (pos:0)
+	int i = 1;
+	t_ptr_trajet temp;
 
+	//verifier si liste est vide
+	if (listes_traj->tete == NULL)
+		return;
+
+	//commencer au debut
+	temp = listes_traj->tete;
+
+	//Si on choisi le premier noeud (tete)
+	if (pos == 0) {
+		//la nouvelle ref de la tete est le noeud suivant
+		listes_traj->tete = temp->suivant;
+		//on supprime l'ancienne tete
+		free(temp);
+		return;
+	}
+
+	/*Si non; si on choisi une autre position: la reference de la tete reste la
+	meme, mais la ref de la queue peut change*/
+
+	//on assigne la queue de nouveau: on commence par le debut
+	listes_traj->queue = listes_traj->tete;
+
+	/*On traverse la liste jusqu'a la fin: temp et queue avance en meme temps.
+	 Les deux commencent par la tete
+	*/
+	while (temp->suivant != NULL) {
+
+		//parcourir la liste avec temp
+		temp = temp->suivant;
+
+		//on a parcouru la liste jusqu'au noeud a la position choisi
+		if (i == pos) {
+			/*on assigne la ref suivant du noeud precedant(queue temporaire) le
+			noeud supprime au noeud suivant le noeud supprime*/
+			listes_traj->queue = temp->suivant;
+			//On supprime le noeud choisi
+			free(temp);
+			return;
+		}
+		
+		/*
+		 on reassigne la referance de la queue au prochain noeud jusqu'a ce
+		 qu'on arrive au vrai dernier noeud change au cas que la queue a ete 
+		 change/supprime
+		*/
+		listes_traj->queue = listes_traj->queue->suivant;
+
+		//incremente la position du noeud courrant
+		i++;
+	}
 }
 
 void ajouter_traj_moyen(t_liste_traj * listes_traj) {
